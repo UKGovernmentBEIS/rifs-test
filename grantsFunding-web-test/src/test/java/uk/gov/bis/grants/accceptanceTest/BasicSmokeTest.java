c306ff2925ece6ac4bc8279f6bc188719f5d74b0
nasirkhanbis <nasir.khan@digital.bis.gov.uk>
2016-11-17 06:30:39 +0000

    updated smoke test runner to inlcude end to end test scenarios

--- grantsFunding-web-test/src/test/java/uk/gov/bis/grants/accceptanceTest/BasicSmokeTest.java
+++ grantsFunding-web-test/src/test/java/uk/gov/bis/grants/accceptanceTest/BasicSmokeTest.java
@@ -7,7 +7,7 @@ import cucumber.api.junit.Cucumber;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		format = { "html:target/cucumber-reports/html", "json:target/cucumber-reports/cucumber.json" }, 
-		features = {"src/test/resources/features.functional/Rifs"},glue="uk.gov.bis.grants.stepdefs",tags={"@Smoke","~@ignore"})
+		features = {"src/test/resources/features.functional/Rifs"},glue="uk.gov.bis.grants.stepdefs",tags={"@Smoke,@End2End","~@ignore"})
 
 
 public class BasicSmokeTest {
