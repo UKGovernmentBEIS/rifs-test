3fb9cc2448872b5816a1c689ad7969e9c8bb2f6c
nasirkhanbis <nasir.khan@digital.bis.gov.uk>
2016-11-18 19:28:07 +0000

    updated Application title to new one in acceptance test

--- grantsFunding-web-test/src/main/java/uk/gov/bis/grants/pagemodel/BasePage.java
+++ grantsFunding-web-test/src/main/java/uk/gov/bis/grants/pagemodel/BasePage.java
@@ -21,7 +21,7 @@ public class BasePage {
 
     private WebDriver driver;
     private String envurl = AppProperties.get("envurl");
-    private String appTitle = "Guidance on grants";
+    private String appTitle = "Research and Innovation Funding Service - RIFS";
    // private deleteElements = "//a[contains(@href,'delete')]";
     //private String appTitle = "Innovation Funding Service - Sign in";
 
