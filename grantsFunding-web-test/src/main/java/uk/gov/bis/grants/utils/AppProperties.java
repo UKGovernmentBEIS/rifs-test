75fceae9f5953a8b8000f4b4a1fb77b48dd4ac23
Doug Clinton <doug@wiredthing.com>
2016-11-22 15:00:01 +0000

    Changed AppProperties to try and find an environment name from an environment variable named "ENV" and use that to identify the config file to use

--- grantsFunding-web-test/src/main/java/uk/gov/bis/grants/utils/AppProperties.java
+++ grantsFunding-web-test/src/main/java/uk/gov/bis/grants/utils/AppProperties.java
@@ -4,42 +4,52 @@ import java.io.FileInputStream;
 import java.util.Properties;
 
 public class AppProperties {
-	 
-    private static Properties properties = null;
- 
-    public static String testclasspath="target/test-classes/";
- 
-    public static Properties getProperties(){
 
-    	if(properties == null){  
-    		System.out.println("Properties file area");
-	        // Check whether the property file location is specified in environment variable "properties"
-	        // otherwise default 
-	        String propertyFile = System.getProperty("properties", System.getProperty("user.dir")+"/src/test/resources/TestConfig-ci.properties");
-	
-	        try {
-	        	System.out.println("Loading properties from file " + propertyFile);
-	            properties=new Properties();
-	            properties.load(new FileInputStream(propertyFile)); 
-	        } catch (Throwable e) {
-	            e.printStackTrace();
-	        } // try
-    	} // if
-        	
+    private static Properties properties = null;
+
+    private static String env = System.getProperty("ENV");
+
+    public static String testclasspath = "target/test-classes/";
+
+    public static Properties getProperties() {
+
+
+        if (properties == null) {
+            System.out.println("Properties file area");
+            // Check whether the property file location is specified in environment variable "properties"
+            // otherwise default
+            final String propertyFile;
+            if (env == null) {
+                propertyFile = System.getProperty("properties", System.getProperty("user.dir") + "/src/test/resources/TestConfig-ci.properties");
+            } else {
+                propertyFile = System.getProperty("properties", System.getProperty("user.dir") + "/src/test/resources/TestConfig-" + env + ".properties");
+            }
+
+            System.out.println("using property file from " + propertyFile);
+
+            try {
+                System.out.println("Loading properties from file " + propertyFile);
+                properties = new Properties();
+                properties.load(new FileInputStream(propertyFile));
+            } catch (Throwable e) {
+                e.printStackTrace();
+            } // try
+        } // if
+
         return properties;
     }
- 
- 
-    public static String get(String propertyKey){
+
+
+    public static String get(String propertyKey) {
         if (propertyKey.equalsIgnoreCase("browser") ||
-               propertyKey.equalsIgnoreCase("env")){
-            
-            if (System.getProperty(propertyKey) != null){
-                return  System.getProperty(propertyKey);
+                propertyKey.equalsIgnoreCase("env")) {
+
+            if (System.getProperty(propertyKey) != null) {
+                return System.getProperty(propertyKey);
             }
         }
- 
-        return  getProperties().getProperty(propertyKey);
+
+        return getProperties().getProperty(propertyKey);
     }
- 
+
 }
